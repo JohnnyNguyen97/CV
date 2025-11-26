eee2061621d49403173e75ba5931fa7093e90fcf
Johnny Hoang Dinh Huy Nguyen <jhdn48823@edu.ucl.dk>
2025-11-26 19:32:21 +0100

    text-center on experience page

--- components/sections/Experience.tsx
+++ components/sections/Experience.tsx
@@ -6,10 +6,10 @@ export default function Experience() {
       </h2>
       <div className="space-y-8 text-zinc-400">
         <div>
-          <h3 className="text-xl font-semibold text-blue-300">
+          <h3 className="text-xl font-semibold text-blue-300 text-center">
             Novo Nordisk — Intern/Developer (2024–2025)
           </h3>
-          <ul className="list-disc list-inside mt-2 space-y-1">
+          <ul className="list-disc list-inside mt-2 space-y-1 text-center">
             <li>Developing a Virtual Reality Application in Unity</li>
             <li>Building C# scripts for NPC functionality</li>
             <li>Working with Azure DevOps Boards and project planning.</li>
