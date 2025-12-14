6a66b63d51c187e5c620bcafc6232f9e018ddf30
Johnny Hoang Dinh Huy Nguyen <jhdn48823@edu.ucl.dk>
2025-12-14 20:11:03 +0100

    Rewrote description

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
   return (
     <main className="w-full">
       <Hero />
-
       <Skills />
       <Tools />
       <Experience />
--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,10 +1,10 @@
 export default function Hero() {
   return (
     <section className="min-h-[10vh] w-full flex flex-col justify-start items-center text-center gap-3 px-8 py-8">
-      <h1 className="text-3xl font-bold title-gradient">Hello, I am Johnny</h1>
+      <h1 className="text-3xl font-bold title-gradient">Portfolio website</h1>
 
       <p className="text-lg max-w-xl text-[color:var(--muted)]">
-        Welcome to my portfolio. This is where I will be showcasing my projects.
+        This is where I will be showcasing my projects and achievements.
       </p>
     </section>
   );
